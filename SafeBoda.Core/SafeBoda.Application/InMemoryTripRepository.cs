497770cabc5e8d07d404704943ee1ddcc3e48524
aline-001 <148322512+aline-001@users.noreply.github.com>
2025-10-07 17:23:10 +0200

    safeboda application

--- SafeBoda.Core/SafeBoda.Application/InMemoryTripRepository.cs
+++ SafeBoda.Core/SafeBoda.Application/InMemoryTripRepository.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using SafeBoda.Core;
+﻿using SafeBoda.Core;
 
 namespace SafeBoda.App
 {
-    class Program
+    class TripRepository
     {
         static void Main(string[] args)
         {
-            // Create a rider
             var rider = new Rider(Guid.NewGuid(), "Aline", "+250788888888");
-
-            // Create a driver
-            var driver = new Driver(Guid.NewGuid(), "John", "+250781111111", "RAE123C");
-
-            // Define locations
-            var start = new Location(-1.9441, 30.0619); // Kigali
+            var driver = new Driver(Guid.NewGuid(), "Asiimwe", "+250781111111", "RAE123C");
+            var start = new Location(-1.9441, 30.0619); 
             var end = new Location(-1.9501, 30.0589);
 
-            // Create a trip
             var trip = new Trip(
                 Guid.NewGuid(),
                 rider.Id,
@@ -28,10 +21,9 @@ namespace SafeBoda.App
                 DateTime.Now
             );
 
-            // Print to console
             Console.WriteLine($"Trip started from {trip.Start} to {trip.End}");
             Console.WriteLine($"Rider: {rider.Name}, Driver: {driver.Name}");
             Console.WriteLine($"Fare: {trip.Fare} RWF");
         }
     }
-}
+} 
