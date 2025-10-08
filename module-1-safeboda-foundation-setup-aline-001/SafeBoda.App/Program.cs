7bd916347823c7817b7e630c491740945c871e03
aline-001 <148322512+aline-001@users.noreply.github.com>
2025-10-08 21:13:04 +0200

    Complete SafeBoda Application

---
+++ module-1-safeboda-foundation-setup-aline-001/SafeBoda.App/Program.cs
@@ -0,0 +1,25 @@
+﻿using SafeBoda.Core;
+using SafeBoda.Application;
+
+namespace SafeBoda.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repo = new InMemoryTripRepository();
+            var trips = repo.GetActiveTrips();
+
+            foreach (Trip trip in trips)
+            {
+                Console.WriteLine($"Trip ID: {trip.Id}");
+                Console.WriteLine($"Driver ID: {trip.DriverId}");
+                Console.WriteLine($"Fare: {trip.Fare}");
+                Console.WriteLine($"Start: {trip.Start}");
+                Console.WriteLine($"End: {trip.End}");
+                Console.WriteLine($"Request Time: {trip.RequestTime}");
+                Console.WriteLine("-------------->------------->--------");
+            }
+        }
+    }
+}
